b87a3f2bcb01a15ff9623203c069e98f4dea648e
Derek Keats <derek@dkeats.com>
2026-08-30 17:27:19 +0200

    Refine Kanban visual hierarchy and actions

--- kanban/templates/layout/kanban_layout_tpl.php
+++ kanban/templates/layout/kanban_layout_tpl.php
@@ -1,5 +1,5 @@
 <?php
-$this->appendArrayVar('headerParams','<link rel="stylesheet" type="text/css" href="'.$this->getResourceUri('kanban.css').'?v=0107" />');
-$this->appendArrayVar('headerParams','<script defer type="text/javascript" src="'.$this->getResourceUri('kanban.js').'?v=0107"></script>');
+$this->appendArrayVar('headerParams','<link rel="stylesheet" type="text/css" href="'.$this->getResourceUri('kanban.css').'?v=0111" />');
+$this->appendArrayVar('headerParams','<script defer type="text/javascript" src="'.$this->getResourceUri('kanban.js').'?v=0111"></script>');
 echo $this->getContent();
 ?>
