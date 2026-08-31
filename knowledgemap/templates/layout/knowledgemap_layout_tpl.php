2c2e7d8a127195485fbd8d89c38677985e5b1ba4
Derek Keats <derek@dkeats.com>
2026-08-31 13:13:32 +0200

    Refresh knowledge map layout asset

--- knowledgemap/templates/layout/knowledgemap_layout_tpl.php
+++ knowledgemap/templates/layout/knowledgemap_layout_tpl.php
@@ -6,6 +6,6 @@
  * @package knowledgemap
  */
 $this->appendArrayVar('headerParams','<link rel="stylesheet" type="text/css" href="'.$this->getResourceUri('knowledgemap.css').'?v=09" />');
-$this->appendArrayVar('headerParams','<script defer type="text/javascript" src="'.$this->getResourceUri('knowledgemap.js').'?v=09"></script>');
+$this->appendArrayVar('headerParams','<script defer type="text/javascript" src="'.$this->getResourceUri('knowledgemap.js').'?v=10"></script>');
 echo $this->getContent();
 ?>
