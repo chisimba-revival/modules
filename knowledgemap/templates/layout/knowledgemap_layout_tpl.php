287d9427d27d4262f62d00607ddea1da70dd578f
Derek Keats <derek@dkeats.com>
2026-08-31 13:16:32 +0200

    Show unsaved state on save icon

--- knowledgemap/templates/layout/knowledgemap_layout_tpl.php
+++ knowledgemap/templates/layout/knowledgemap_layout_tpl.php
@@ -5,7 +5,7 @@
  * @author Derek Keats
  * @package knowledgemap
  */
-$this->appendArrayVar('headerParams','<link rel="stylesheet" type="text/css" href="'.$this->getResourceUri('knowledgemap.css').'?v=09" />');
-$this->appendArrayVar('headerParams','<script defer type="text/javascript" src="'.$this->getResourceUri('knowledgemap.js').'?v=10"></script>');
+$this->appendArrayVar('headerParams','<link rel="stylesheet" type="text/css" href="'.$this->getResourceUri('knowledgemap.css').'?v=11" />');
+$this->appendArrayVar('headerParams','<script defer type="text/javascript" src="'.$this->getResourceUri('knowledgemap.js').'?v=11"></script>');
 echo $this->getContent();
 ?>
